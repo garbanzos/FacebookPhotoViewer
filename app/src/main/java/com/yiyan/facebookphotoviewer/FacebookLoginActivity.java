697b295737b645334d5821bfeed54987f5307753
garbanzos <yiyant@gmail.com>
2016-05-27 15:25:52 +0800

    Added another activity and set up facebook sdk

--- app/src/main/java/com/yiyan/facebookphotoviewer/FacebookLoginActivity.java
+++ app/src/main/java/com/yiyan/facebookphotoviewer/FacebookLoginActivity.java
@@ -2,6 +2,8 @@ package com.yiyan.facebookphotoviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class FacebookLoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class FacebookLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_login);
+        FacebookSdk.sdkInitialize(getApplicationContext());
     }
 }
