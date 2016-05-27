705cf8809c90046508cd911d893a68b5c736680f
garbanzos <yiyant@gmail.com>
2016-05-27 16:19:21 +0800

    Facebook login and logout now works

--- app/src/main/java/com/yiyan/facebookphotoviewer/MainActivity.java
+++ app/src/main/java/com/yiyan/facebookphotoviewer/MainActivity.java
@@ -1,7 +1,14 @@
 package com.yiyan.facebookphotoviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.login.LoginManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle inBundle = getIntent().getExtras();
+        String name = inBundle.get("name").toString();
+        String surname = inBundle.get("surname").toString();
+        String imageUrl = inBundle.get("imageUrl").toString();
+
+        TextView nameView = (TextView)findViewById(R.id.nameAndSurname);
+        nameView.setText("" + name + " " + surname);
+        new DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            logout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void logout(){
+        LoginManager.getInstance().logOut();
+        Intent login = new Intent(MainActivity.this, FacebookLoginActivity.class);
+        startActivity(login);
+        finish();
     }
 }
