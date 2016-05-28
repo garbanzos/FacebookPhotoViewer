832d9695cd1f915de112786afa79f4fd32ecc4c6
garbanzos <yiyant@gmail.com>
2016-05-28 14:24:38 +0800

    Managed to get album ids and display one picture

--- app/src/main/java/com/yiyan/facebookphotoviewer/MainActivity.java
+++ app/src/main/java/com/yiyan/facebookphotoviewer/MainActivity.java
@@ -3,14 +3,25 @@ package com.yiyan.facebookphotoviewer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.login.LoginManager;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    JSONArray albumJsonArray = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,34 @@ public class MainActivity extends AppCompatActivity {
 
         TextView nameView = (TextView)findViewById(R.id.nameAndSurname);
         nameView.setText("" + name + " " + surname);
-        new DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
+
+        // get albums
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/albums",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        JSONObject jsonObject = response.getJSONObject();
+                        try {
+                            albumJsonArray = jsonObject.getJSONArray("data");
+                            for(int i = 0; i < albumJsonArray.length(); i++){
+                                JSONObject oneAlbum = albumJsonArray.getJSONObject(i);
+                                Log.d("ALBUMS", oneAlbum.getString("name"));
+                                Log.d("ALBUM ID", oneAlbum.getString("id"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
+
+        // get photos
+
+
+        new DownloadImage((ImageView)findViewById(R.id.profileImage)).execute("https://graph.facebook.com/865787606822791/picture?type=normal");
     }
 
     @Override
